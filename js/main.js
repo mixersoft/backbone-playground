8a8c7a041a0d5d44eae2d3ca8f0b8dbed5b3dc4c
michael <michael@snaphappi.com>
2013-08-13 17:02:20 -0400

    add static data and data parsing for gallery/thumbnails

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+$(function() {
+	// same as json.response from $ajax({dataType: 'json'}).success.call(this, json, status, o);
+	var json = {
+		response: JSON.parse(SNAPPI.CFG.JSON.raw),
+	}
+	var cc = json.response['response'].castingCall;
+	var thumbnails = SNAPPI.parseCC(cc);
+	var check;
+	
+});
+
 
